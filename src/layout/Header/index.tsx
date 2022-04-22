af64103fa2a85200872bf738dbccc82d9ffb2e06
wuchangyun <1725625529@qq.com>
2022-04-22 21:33:22 +0800

    4-22

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -6,17 +6,20 @@ const Header:FC = () => {
     return (
         <div>
             <div className={styles.top}></div>
-            <div className={styles.header}>
-                <div className={styles.avator}>
-                    图标
-                    <img src="" alt="" />
-                </div>
-                <div className={styles.router}>
-                    <Link to="/">首页</Link>
-                    <Link to="/blog">博客</Link>
-                    <Link to="/manage">管理</Link>
-                    <Link to="/join">加入我们</Link>
-                    <Link to="/about">关于团队</Link>
+            <div className={styles.header_F}>
+
+                <div className={styles.header}>
+                    <div className={styles.avator}>
+                        图标
+                        <img src="" alt="" />
+                    </div>
+                    <div className={styles.router}>
+                        <Link to="/">首页</Link>
+                        <Link to="/blog">技术沙龙</Link>
+                        <Link to="/manage">开放平台</Link>
+                        <Link to="/join">加入我们</Link>
+                        <Link to="/about">关于团队</Link>
+                    </div>
                 </div>
             </div>
         </div>
