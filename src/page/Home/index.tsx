1ae9358695d0fff28780fdae48d6066c9bac6c90
wuchangyun <1725625529@qq.com>
2022-05-23 21:27:19 +0800

    feat

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -16,7 +16,7 @@ const Home: FC = () => {
                             <p className={styles.font_14} style={{opacity: 1}}>团队服务平台</p>
                         </div>
                     </div>
-                    <video className={styles.video_source} autoPlay={true} loop={true} height="381" width="1920">
+                    <video className={styles.video_source} muted={true} autoPlay={true} loop={true} height="381" width="1920">
                         <source src="https://storage.360buyimg.com/jdrd-misc/index/video/video.mp4" type="video/mp4"/>
                     </video>
                 </div>
